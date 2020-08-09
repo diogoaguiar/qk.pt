1b11a6cc9ed05ce5df8a48d6514fb817ee383a10
Diogo Aguiar <diogo@dg7.pt>
2020-08-09 16:26:09 +0100

    Add 404 and 400 error pages. Bump npm dependencies version.

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,31 @@
 'use strict';
 
 const express = require('express');
+const mpath = require('path');
 const { Datastore } = require('@google-cloud/datastore');
 
 const router = express.Router();
 const datastore = new Datastore();
 
 const env = process.env.ENVIRONMENT || 'dev';
+const views = mpath.join(__dirname, '../resources/views');
 
 router.all('/:path', async (req, res) => {
     let query = datastore.createQuery(env, 'Url');
     let { path } = req.params;
 
     if (!path.match(/^[a-zA-Z0-9\-\_]+$/)) {
-        return res.status(400).send('Invalid path');
+        return res.status(400).sendFile(mpath.join(views, '400.html'));
     }
 
     let [urls] = await query.filter('path', path).run();
     if (!urls.length) {
-        return res.status(404).send('Not found');
+        return res.status(404).sendFile(mpath.join(views, '404.html'));
     }
 
     let url = urls[0]['redirect'];
     if (!isValidUrl(url)) {
-        return res.status(400).send('Invalid URL');
+        return res.status(400).sendFile(mpath.join(views, '400.html'));
     }
 
     return res.redirect(301, url);
